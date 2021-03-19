2fc7981a59a89eb6e85c208ff6b83f2acc5105fb
goodbye-isaac <goodbyeisaac1727@gmail.com>
2021-03-19 17:47:22 +0900

    絞り込み条件、scope,フィルタ、auto_link

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,8 +4,8 @@ class Task < ApplicationRecord
   validates :name, length: { maximum: 30}
   validate :validate_name_not_including_comma
 
-  belongs_to :user
-
+  belongs_to :User
+  scope :recent, -> { order(created_at: :desc) }
   private
 
   def validate_name_not_including_comma
